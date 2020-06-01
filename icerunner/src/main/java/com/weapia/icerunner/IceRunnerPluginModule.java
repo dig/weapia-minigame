9c4cf4874aa927d6521171d849626e3348eee420
Joseph <joseph42@live.co.uk>
2020-06-01 14:07:12 +0100

    Fix icerunner

--- icerunner/src/main/java/com/weapia/icerunner/IceRunnerPluginModule.java
+++ icerunner/src/main/java/com/weapia/icerunner/IceRunnerPluginModule.java
@@ -1,5 +1,6 @@
 package com.weapia.icerunner;
 
+import net.sunken.common.inject.PluginFacetBinder;
 import net.sunken.core.CoreModule;
 import net.sunken.core.engine.EngineModule;
 import net.sunken.core.inject.PluginModule;
@@ -15,6 +16,9 @@ public class IceRunnerPluginModule extends PluginModule {
     public void configurePlugin() {
         install(new CoreModule());
         install(new EngineModule());
+
+        final PluginFacetBinder pluginFacetBinder = new PluginFacetBinder(binder());
+        pluginFacetBinder.addBinding(GlobalListener.class);
     }
 
 }
