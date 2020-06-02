cb877afc65f1b1e5c2c27f16c64742fce654de3d
Joseph <joseph42@live.co.uk>
2020-06-02 03:11:24 +0100

    Remove old scoreboardManager

--- icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayer.java
+++ icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayer.java
@@ -1,14 +1,13 @@
 package com.weapia.icerunner.player;
 
 import net.sunken.core.player.CorePlayer;
-import net.sunken.core.scoreboard.ScoreboardManager;
 
 import java.util.UUID;
 
 public class MinigamePlayer extends CorePlayer {
 
-    public MinigamePlayer(UUID uuid, String username, ScoreboardManager scoreboardManager) {
-        super(uuid, username, scoreboardManager);
+    public MinigamePlayer(UUID uuid, String username) {
+        super(uuid, username);
     }
 
     @Override
--- icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayerFactory.java
+++ icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayerFactory.java
@@ -1,17 +1,11 @@
 package com.weapia.icerunner.player;
 
-import com.google.inject.Inject;
-import net.sunken.core.scoreboard.ScoreboardManager;
-
 import java.util.UUID;
 
 public class MinigamePlayerFactory {
 
-    @Inject
-    private ScoreboardManager scoreboardManager;
-
     public MinigamePlayer createPlayer(UUID uuid, String username) {
-        return new MinigamePlayer(uuid, username, scoreboardManager);
+        return new MinigamePlayer(uuid, username);
     }
 
 }
