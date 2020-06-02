84bdef049228fa1457cc2704fc6209664d493aec
Joseph <joseph42@live.co.uk>
2020-06-02 03:31:19 +0100

    Add scoreboardRegistry to minigamePlayer

--- icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayer.java
+++ icerunner/src/main/java/com/weapia/icerunner/player/MinigamePlayer.java
@@ -1,13 +1,14 @@
 package com.weapia.icerunner.player;
 
 import net.sunken.core.player.CorePlayer;
+import net.sunken.core.scoreboard.ScoreboardRegistry;
 
 import java.util.UUID;
 
 public class MinigamePlayer extends CorePlayer {
 
-    public MinigamePlayer(UUID uuid, String username) {
-        super(uuid, username);
+    public MinigamePlayer(UUID uuid, String username, ScoreboardRegistry scoreboardRegistry) {
+        super(uuid, username, scoreboardRegistry);
     }
 
     @Override
