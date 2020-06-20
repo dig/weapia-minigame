411678bf95c4f6b5c6aaea7edfe3bfe5b1f1a166
Node <35534146+node-mc@users.noreply.github.com>
2020-06-20 03:56:59 +0100

    start worldmanager

---
+++ survivalrealms/src/main/java/com/weapia/survivalrealms/world/WorldManager.java
@@ -0,0 +1,87 @@
+package com.weapia.survivalrealms.world;
+
+import net.sunken.common.inject.*;
+import net.sunken.common.server.World;
+import net.sunken.core.util.*;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
+import org.bukkit.event.player.*;
+import org.bukkit.plugin.java.*;
+import org.bukkit.scheduler.*;
+
+import javax.inject.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+@Singleton
+public class WorldManager implements Facet, Enableable, Listener {
+
+    private static final long UNLOAD_AFTER_TICKS_OFFLINE = Ticks.from(1, TimeUnit.MINUTES);
+
+    @Inject
+    private JavaPlugin plugin;
+
+    private final Map<UUID, World> loadedWorlds = new HashMap<>();
+    private final Map<UUID, BukkitTask> scheduledUnloadWorlds = new HashMap<>();
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (checkScheduledUnloadWorld(player)) {
+            unscheduleUnloadWorld(player);
+        } else {
+            loadWorld(player);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        scheduleUnloadWorld(player);
+    }
+
+    private void loadWorld(Player player) {
+    }
+
+    private boolean checkScheduledUnloadWorld(Player player) {
+        return loadedWorlds.containsKey(player.getUniqueId());
+    }
+
+    private void scheduleUnloadWorld(Player player) {
+        BukkitTask unloadWorldTask = Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            if (!player.isOnline()) {
+                unloadWorld(player.getUniqueId());
+            }
+        }, UNLOAD_AFTER_TICKS_OFFLINE);
+
+        scheduledUnloadWorlds.put(player.getUniqueId(), unloadWorldTask);
+    }
+
+    private void unscheduleUnloadWorld(Player player) {
+        BukkitTask unloadWorldTask = scheduledUnloadWorlds.remove(player.getUniqueId());
+        if (unloadWorldTask != null && !unloadWorldTask.isCancelled()) {
+            unloadWorldTask.cancel();
+        }
+    }
+
+    private void unloadWorld(UUID playerUUID) {
+    }
+
+    private void unloadAllWorlds() {
+        for (UUID playerUUID : loadedWorlds.keySet()) {
+            unloadWorld(playerUUID);
+        }
+    }
+
+    @Override
+    public void enable() {
+    }
+
+    @Override
+    public void disable() {
+        unloadAllWorlds();
+    }
+}
