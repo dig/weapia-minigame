2c6b99bc00de655654169d7b1a58992d1c62eb6e
Digital <josephpro42@live.co.uk>
2020-06-20 14:31:15 +0100

    Keep track of location

--- survivalrealms/src/main/java/com/weapia/survivalrealms/player/SurvivalPlayer.java
+++ survivalrealms/src/main/java/com/weapia/survivalrealms/player/SurvivalPlayer.java
@@ -1,28 +1,30 @@
 package com.weapia.survivalrealms.player;
 
+import lombok.Getter;
 import lombok.NonNull;
 import net.sunken.common.database.DatabaseHelper;
 import net.sunken.common.player.Rank;
 import net.sunken.core.PluginInform;
 import net.sunken.core.player.CorePlayer;
-import net.sunken.core.scoreboard.CustomScoreboard;
 import net.sunken.core.scoreboard.ScoreboardRegistry;
+import net.sunken.core.util.MongoUtil;
 import org.bson.Document;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class SurvivalPlayer extends CorePlayer {
 
-    private PluginInform pluginInform;
-
+    @Getter
+    private Location lastLocation;
     private String world;
 
     public SurvivalPlayer(UUID uuid, String username, ScoreboardRegistry scoreboardRegistry, PluginInform pluginInform) {
-        super(uuid, username, scoreboardRegistry);
-        this.pluginInform = pluginInform;
+        super(uuid, username, scoreboardRegistry, pluginInform);
         this.world = null;
+        this.lastLocation = null;
     }
 
     @Override
@@ -30,23 +32,12 @@ public class SurvivalPlayer extends CorePlayer {
         super.setup(player);
         player.getInventory().clear();
 
-        CustomScoreboard customScoreboard = new CustomScoreboard(ChatColor.AQUA + "" + ChatColor.BOLD + "WEAPIA");
-        customScoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 5);
-
-        customScoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank", 4);
-        customScoreboard.createEntry("RankValue", rank == Rank.PLAYER ? ChatColor.RED + "No Rank" : ChatColor.valueOf(rank.getColour()) + "" + rank.getFriendlyName(), 3);
-        customScoreboard.createEntry("Spacer2", ChatColor.BLACK + " ", 2);
-
-        customScoreboard.createEntry("ServerID", ChatColor.GRAY + pluginInform.getServer().getId(), 1);
-        customScoreboard.createEntry("URL", ChatColor.LIGHT_PURPLE + "play.weapia.com", 0);
-
-        customScoreboard.add(player);
-        scoreboardRegistry.register(player.getUniqueId().toString(), customScoreboard);
-    }
-
-    @Override
-    public void destroy(@NonNull Player player) {
-        scoreboardRegistry.unregister(this.uuid.toString());
+        setScoreboard(player, scoreboard -> {
+            scoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 5);
+            scoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank", 4);
+            scoreboard.createEntry("RankValue", rank == Rank.PLAYER ? ChatColor.RED + "No Rank" : ChatColor.valueOf(rank.getColour()) + "" + rank.getFriendlyName(), 3);
+            scoreboard.createEntry("Spacer2", ChatColor.BLACK + " ", 2);
+        });
     }
 
     @Override
@@ -55,6 +46,7 @@ public class SurvivalPlayer extends CorePlayer {
         if (document.containsKey(DatabaseHelper.PLAYER_SURVIVAL_REALMS_KEY)) {
             Document doc = (Document) document.get(DatabaseHelper.PLAYER_SURVIVAL_REALMS_KEY);
             world = doc.getString(DatabaseHelper.PLAYER_SURVIVAL_REALMS_WORLD_KEY);
+            lastLocation = MongoUtil.location((Document) doc.get(DatabaseHelper.PLAYER_SURVIVAL_REALMS_LOCATION_KEY));
         }
         return true;
     }
@@ -63,6 +55,8 @@ public class SurvivalPlayer extends CorePlayer {
     public Document toDocument() {
         Document document = new Document()
                 .append(DatabaseHelper.PLAYER_SURVIVAL_REALMS_WORLD_KEY, world);
+        toPlayer().ifPresent(player -> document.append(DatabaseHelper.PLAYER_SURVIVAL_REALMS_LOCATION_KEY, MongoUtil.location(player.getLocation())));
+
         return super.toDocument()
                 .append(DatabaseHelper.PLAYER_SURVIVAL_REALMS_KEY, document);
     }
