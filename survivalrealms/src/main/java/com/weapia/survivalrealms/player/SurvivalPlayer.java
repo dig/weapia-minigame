8cd73f49e5cd917b4a9049cc402ed8f0b11ae470
Digital <josephpro42@live.co.uk>
2020-06-20 14:01:23 +0100

    Refactor setup and destroy

--- survivalrealms/src/main/java/com/weapia/survivalrealms/player/SurvivalPlayer.java
+++ survivalrealms/src/main/java/com/weapia/survivalrealms/player/SurvivalPlayer.java
@@ -1,5 +1,6 @@
 package com.weapia.survivalrealms.player;
 
+import lombok.NonNull;
 import net.sunken.common.database.DatabaseHelper;
 import net.sunken.common.player.Rank;
 import net.sunken.core.PluginInform;
@@ -10,7 +11,6 @@ import org.bson.Document;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public class SurvivalPlayer extends CorePlayer {
@@ -26,30 +26,26 @@ public class SurvivalPlayer extends CorePlayer {
     }
 
     @Override
-    public void setup() {
-        super.setup();
-        Optional<? extends Player> playerOptional = toPlayer();
-        if (playerOptional.isPresent()) {
-            Player player = playerOptional.get();
-            player.getInventory().clear();
+    public void setup(@NonNull Player player) {
+        super.setup(player);
+        player.getInventory().clear();
 
-            CustomScoreboard customScoreboard = new CustomScoreboard(ChatColor.AQUA + "" + ChatColor.BOLD + "WEAPIA");
-            customScoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 5);
+        CustomScoreboard customScoreboard = new CustomScoreboard(ChatColor.AQUA + "" + ChatColor.BOLD + "WEAPIA");
+        customScoreboard.createEntry("Spacer1", ChatColor.WHITE + " ", 5);
 
-            customScoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank", 4);
-            customScoreboard.createEntry("RankValue", rank == Rank.PLAYER ? ChatColor.RED + "No Rank" : ChatColor.valueOf(rank.getColour()) + "" + rank.getFriendlyName(), 3);
-            customScoreboard.createEntry("Spacer2", ChatColor.BLACK + " ", 2);
+        customScoreboard.createEntry("RankTitle", ChatColor.WHITE + "Rank", 4);
+        customScoreboard.createEntry("RankValue", rank == Rank.PLAYER ? ChatColor.RED + "No Rank" : ChatColor.valueOf(rank.getColour()) + "" + rank.getFriendlyName(), 3);
+        customScoreboard.createEntry("Spacer2", ChatColor.BLACK + " ", 2);
 
-            customScoreboard.createEntry("ServerID", ChatColor.GRAY + pluginInform.getServer().getId(), 1);
-            customScoreboard.createEntry("URL", ChatColor.LIGHT_PURPLE + "play.weapia.com", 0);
+        customScoreboard.createEntry("ServerID", ChatColor.GRAY + pluginInform.getServer().getId(), 1);
+        customScoreboard.createEntry("URL", ChatColor.LIGHT_PURPLE + "play.weapia.com", 0);
 
-            customScoreboard.add(player);
-            scoreboardRegistry.register(player.getUniqueId().toString(), customScoreboard);
-        }
+        customScoreboard.add(player);
+        scoreboardRegistry.register(player.getUniqueId().toString(), customScoreboard);
     }
 
     @Override
-    public void destroy() {
+    public void destroy(@NonNull Player player) {
         scoreboardRegistry.unregister(this.uuid.toString());
     }
 
