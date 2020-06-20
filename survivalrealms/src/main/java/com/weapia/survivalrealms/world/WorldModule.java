3713a3a88967e335739971ad966a3cd91a3ac4b4
Digital <josephpro42@live.co.uk>
2020-06-20 14:39:30 +0100

    Test

--- survivalrealms/src/main/java/com/weapia/survivalrealms/world/WorldModule.java
+++ survivalrealms/src/main/java/com/weapia/survivalrealms/world/WorldModule.java
@@ -10,5 +10,4 @@ public class WorldModule extends AbstractModule {
         final PluginFacetBinder pluginFacetBinder = new PluginFacetBinder(binder());
         pluginFacetBinder.addBinding(WorldManager.class);
     }
-
 }
